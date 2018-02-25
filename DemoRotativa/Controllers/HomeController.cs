efaf504268e91a599204607c5b2c58171168bfb1
kalpeshsatasiya <satasiya.kalpesh2006@gmail.com>
2018-02-25 10:22:45 +0530

    intial commit

---
+++ DemoRotativa/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DemoRotativa.Models;
+using Rotativa.AspNetCore;
+
+namespace DemoRotativa.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customers = GetCustomers();
+            return new ViewAsPdf("Index", customers);
+        }
+
+        private List<Customer> GetCustomers()
+        {
+            return new List<Customer>()
+            {
+                new Customer(){ Name="Kalpesh", City="Ahmedabad"},
+                new Customer(){ Name="Vijay", City="Surat"},
+                new Customer(){ Name="John", City="New York"},
+                new Customer(){ Name="Smith", City="Parth"},
+            };
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DemoRotativa/Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace DemoRotativa.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}
